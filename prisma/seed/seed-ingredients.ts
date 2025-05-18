4c1367bdb8a4feca91d5f59d788ae3d12405d957
pf <peterbugiel@gmail.com>
2025-05-18 22:36:22 +0200

    chore(database): Ensure that ingredient names are unique.

--- prisma/seed/seed-ingredients.ts
+++ prisma/seed/seed-ingredients.ts
@@ -1,12 +1,15 @@
 import { faker } from "@faker-js/faker";
 import { PrismaClient, User } from "@prisma/client";
+import { UniqueEnforcer } from "enforce-unique";
 
 export const seedIngredients = async (prisma: PrismaClient, pizzaboy: User) => {
+  const uniqueEnforcer = new UniqueEnforcer();
+
   await Promise.all(
     Array.from({ length: 50 }, () =>
       prisma.ingredient.create({
         data: {
-          name: faker.food.ingredient(),
+          name: uniqueEnforcer.enforce(() => faker.food.ingredient()),
           user: {
             connect: pizzaboy,
           },
