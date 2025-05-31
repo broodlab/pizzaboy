73b42cf4598fb2ad8b04c3e6d06dfc075094ae68
pf <peterbugiel@gmail.com>
2025-05-31 23:13:03 +0200

    feat(ingredient creation): Get the 'name must be unique' validation feedback.

--- src/routes/ingredients_.new/index.tsx
+++ src/routes/ingredients_.new/index.tsx
@@ -3,18 +3,33 @@ import type { Route } from "./+types/";
 import { useForm } from "@conform-to/react";
 import { parseWithZod } from "@conform-to/zod";
 import prisma from "~/utils/prisma";
-import { redirect } from "react-router";
+import { data, redirect } from "react-router";
 import { foodCategories } from "~/types/food-categories";
 
-const schema = z.object({
+const ingredientSchema = z.object({
   category: z.string().optional(),
   description: z.string().optional(),
-  name: z.string(),
+  name: z.string().refine(async (name) => {
+    const count = await prisma.ingredient.count({
+      where: { name },
+    });
+    return count === 0;
+  }, "Unique"),
 });
 
 export const action = async ({ request }: Route.ActionArgs) => {
   const formData = await request.formData();
-  const submission = parseWithZod(formData, { schema });
+  const submission = await parseWithZod(formData, {
+    async: true,
+    schema: ingredientSchema,
+  });
+
+  if (submission.value === undefined) {
+    return data({ submission } as const, {
+      status: 400,
+    });
+  }
+
   const { category, description, name } = submission.value;
   await prisma.ingredient.create({
     data: {
@@ -36,9 +51,9 @@ export default function IngredientCreation({
   actionData,
 }: Route.ComponentProps) {
   const [form, fields] = useForm({
-    lastResult: actionData,
+    lastResult: actionData?.submission,
     onValidate({ formData }) {
-      return parseWithZod(formData, { schema });
+      return parseWithZod(formData, { schema: ingredientSchema });
     },
     shouldValidate: "onBlur",
   });
