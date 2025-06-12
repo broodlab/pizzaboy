c3c83d71c1e4e97c496d1c0b21d84958cab8fe1e
pf <peterbugiel@gmail.com>
2025-06-12 22:00:04 +0200

    chore(project): Rename 'app.css' file to 'application.css'.

--- application/root.tsx
+++ application/root.tsx
@@ -8,7 +8,7 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
-import "./app.css";
+import "./application.css";
 
 export const meta: Route.MetaFunction = () => [
   {
