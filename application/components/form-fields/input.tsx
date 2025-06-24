5c17edda3aa8659f8838ef5f591c40ce6f7aa161
pf <peterbugiel@gmail.com>
2025-06-24 21:20:04 +0200

    feat(application): shadcn/ui based 'Input' component.

--- application/components/form-fields/input.tsx
+++ application/components/form-fields/input.tsx
@@ -1,14 +1,20 @@
-import type { FC, InputHTMLAttributes } from "react";
+import type { ComponentProps, FC } from "react";
 import { cn } from "~/utils/create-class-name";
 
-export const Input: FC<InputHTMLAttributes<HTMLInputElement>> = ({
+export const Input: FC<ComponentProps<"input">> = ({
   className,
   type,
   ...props
 }) => {
   return (
     <input
-      className={cn("border-1 border-gray-200", className)}
+      className={cn(
+        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+        className,
+      )}
+      data-slot="input"
       type={type}
       {...props}
     />
