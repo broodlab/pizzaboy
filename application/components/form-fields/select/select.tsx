593ed46a4bdd51525f698d8df092fb4dc94fe02a
pf <peterbugiel@gmail.com>
2025-06-26 19:54:52 +0200

    fix('Select' component): Fix the type incompatibility of the 'defaultValue' property between Conform and Radix.

--- application/components/form-fields/select/select.tsx
+++ application/components/form-fields/select/select.tsx
@@ -2,8 +2,30 @@ import * as React from "react";
 import { type ComponentProps, type FC } from "react";
 import * as SelectPrimitive from "@radix-ui/react-select";
 
-export const Select: FC<ComponentProps<typeof SelectPrimitive.Root>> = ({
-  ...props
-}) => {
-  return <SelectPrimitive.Root data-slot="select" {...props} />;
+type ConformJsDefaultValue = undefined | number | readonly string[] | string;
+
+type SelectProps = Omit<
+  ComponentProps<typeof SelectPrimitive.Root>,
+  "defaultValue"
+> & {
+  defaultValue?: ConformJsDefaultValue;
+};
+
+export const Select: FC<SelectProps> = ({ defaultValue, ...props }) => {
+  return (
+    <SelectPrimitive.Root
+      data-slot="select"
+      {...props}
+      defaultValue={mapToCorrectType(defaultValue)}
+    />
+  );
+};
+
+const mapToCorrectType = (
+  defaultValue: ConformJsDefaultValue,
+): undefined | string => {
+  if (Array.isArray(defaultValue)) {
+    throw new Error("The 'defaultValue' property must not be an array.");
+  }
+  return defaultValue?.toString();
 };
