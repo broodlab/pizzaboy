a62e97a7aa6ce16915a57092a32d03801ecc8635
pf <peterbugiel@gmail.com>
2025-07-08 22:28:30 +0200

    refactor: Bring the 'Separator' component into right shape.

--- application/components/separator.tsx
+++ application/components/separator.tsx
@@ -1,28 +1,25 @@
-"use client";
-
+import type { ComponentProps, FC } from "react";
 import * as React from "react";
 import * as SeparatorPrimitive from "@radix-ui/react-separator";
 
 import { cn } from "~/utils/create-class-name";
 
-function Separator({
+export const Separator: FC<ComponentProps<typeof SeparatorPrimitive.Root>> = ({
   className,
-  orientation = "horizontal",
   decorative = true,
+  orientation = "horizontal",
   ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+}) => {
   return (
     <SeparatorPrimitive.Root
-      data-slot="separator"
-      decorative={decorative}
-      orientation={orientation}
       className={cn(
         "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
         className,
       )}
+      data-slot="separator"
+      decorative={decorative}
+      orientation={orientation}
       {...props}
     />
   );
-}
-
-export { Separator };
+};
