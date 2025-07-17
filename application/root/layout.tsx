0a38b326ce02a153facca46ed85c41a287af4c92
pf <peterbugiel@gmail.com>
2025-07-17 21:43:09 +0200

    refactor: Move the sidebar to the layout.

--- application/root/layout.tsx
+++ application/root/layout.tsx
@@ -1,10 +1,40 @@
 import type { FC, PropsWithChildren } from "react";
-import { SidebarProvider, SidebarTrigger } from "~/components/sidebar";
-import { PizzaboySidebar } from "~/components/pizzaboy-sidebar";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "~/components/sidebar";
+import { Link } from "react-router";
 
 export const Layout: FC<PropsWithChildren> = ({ children }) => (
   <SidebarProvider>
-    <PizzaboySidebar />
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <Link to="/ingredients/create">Create Ingredient</Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <Link to="/ingredients">Ingredients</Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
     <header className="flex h-12 items-center justify-between px-4">
       <SidebarTrigger />
     </header>
