f0b8cd2e92ad303cd59ba358cdf9863b04b50982
pf <peterbugiel@gmail.com>
2025-07-26 11:58:06 +0200

    feat(ingredient schema): Make the 'category' property non-optional.

--- application/features/ingredients/common/schemas/index.ts
+++ application/features/ingredients/common/schemas/index.ts
@@ -5,9 +5,9 @@ import { descriptionMaxLength, nameMaxLength } from "~/configs/schema-rules";
 export const ingredientNameSchema = z.string().max(nameMaxLength);
 
 export const ingredientSchema = z.object({
-  category: z
-    .union(foodCategories.map((foodCategory) => z.literal(foodCategory)))
-    .optional(),
+  category: z.union(
+    foodCategories.map((foodCategory) => z.literal(foodCategory)),
+  ),
   description: z.string().max(descriptionMaxLength).optional(),
   name: ingredientNameSchema,
 });
