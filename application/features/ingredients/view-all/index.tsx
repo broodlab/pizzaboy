4115031c711a9b63aff33eebd3cf7420def6280c
pf <peterbugiel@gmail.com>
2025-08-01 20:44:43 +0200

    style('Ingredients' page): Display the ingredients on a table.

--- application/features/ingredients/view-all/index.tsx
+++ application/features/ingredients/view-all/index.tsx
@@ -1,6 +1,14 @@
 import type { Route } from "./+types/";
 import prisma from "~/utils/prisma";
 import { Link } from "react-router";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/table";
 
 export const loader = () => {
   return prisma.ingredient.findMany();
@@ -8,22 +16,37 @@ export const loader = () => {
 
 export default function Ingredients({ loaderData }: Route.ComponentProps) {
   return (
-    <ul>
-      {loaderData.map(({ category, id, name }) => (
-        <li key={id}>
-          <span>
-            {name} ({category})
-          </span>
-          <span>&nbsp;</span>
-          <Link to={`${id}/edit`} relative="route">
-            Edit
-          </Link>
-          <span>&nbsp;</span>
-          <Link to={`${id}/delete`} relative="route">
-            Delete
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <h1 className="text-lg font-semibold">Ingredients</h1>
+      <p className="text-muted-foreground text-sm text-balance">
+        Search and manage your ingredients.
+      </p>
+      <Table className="w-lg">
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Category</TableHead>
+            <TableHead></TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {loaderData.map(({ category, id, name }) => (
+            <TableRow key={id}>
+              <TableCell className="max-w-xs truncate">{name}</TableCell>
+              <TableCell>{category}</TableCell>
+              <TableCell>
+                <Link to={`${id}/edit`} relative="route">
+                  Edit
+                </Link>
+                <span>&nbsp;</span>
+                <Link to={`${id}/delete`} relative="route">
+                  Delete
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
   );
 }
