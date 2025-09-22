7ab841447b57e04ff1da3eca29478f46a70763bd
pf <peterbugiel@gmail.com>
2025-09-22 22:30:14 +0200

    style('Card' component): Reduce the rounding of the corners.

--- application/components/card/card.tsx
+++ application/components/card/card.tsx
@@ -5,7 +5,7 @@ export const Card: FC<ComponentProps<"div">> = ({ className, ...props }) => {
   return (
     <div
       className={cn(
-        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
+        "bg-card text-card-foreground flex flex-col gap-6 rounded-md border py-6 shadow-sm",
         className,
       )}
       data-slot="card"
