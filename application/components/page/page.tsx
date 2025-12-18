9076b08395d9ac33a54e9f784fec896fe855f5ea
pf <peterbugiel@gmail.com>
2025-12-18 23:23:10 +0100

    style('Page' component): Use the full width on small screen sizes.

--- application/components/page/page.tsx
+++ application/components/page/page.tsx
@@ -3,7 +3,9 @@ import type { FC, PropsWithChildren } from "react";
 export const Page: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="flex flex-col items-center">
-      <div className="flex w-lg flex-col gap-8">{children}</div>
+      <div className="flex w-full flex-col gap-8 lg:w-lg xl:w-xl 2xl:w-2xl">
+        {children}
+      </div>
     </div>
   );
 };
