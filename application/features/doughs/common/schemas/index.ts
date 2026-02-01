eb41e57761649b33013815ed299861c5fc74fbc2
pf <peterbugiel@gmail.com>
2026-02-01 09:28:38 +0000

    feat('Dough Creation' page): 'description' property specification.

--- application/features/doughs/common/schemas/index.ts
+++ application/features/doughs/common/schemas/index.ts
@@ -1,8 +1,9 @@
 import { z } from "zod/v4";
-import { nameMaxLength } from "~/configs/schema-rules";
+import { descriptionMaxLength, nameMaxLength } from "~/configs/schema-rules";
 
 export const doughNameSchema = z.string().max(nameMaxLength);
 
 export const doughSchema = z.object({
+  description: z.string().max(descriptionMaxLength).optional(),
   name: doughNameSchema,
 });
--- application/features/doughs/create/index.tsx
+++ application/features/doughs/create/index.tsx
@@ -1,7 +1,18 @@
 import { Page, PageHeader, PageIntro, PageTitle } from "~/components/page";
 import { Form, redirect } from "react-router";
-import { getFormProps, getInputProps, useForm } from "@conform-to/react";
-import { Card, CardContent } from "~/components/card";
+import {
+  getFormProps,
+  getInputProps,
+  getTextareaProps,
+  useForm,
+} from "@conform-to/react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/card";
 import { Label } from "~/components/label";
 import { Input } from "~/components/input";
 import { Button } from "~/components/button";
@@ -10,6 +21,7 @@ import { doughSchema } from "~/features/doughs/common/schemas";
 import type { Route } from "./+types";
 import prisma from "~/utils/prisma.server";
 import { ErrorList } from "~/components/error-list";
+import { Textarea } from "~/components/textarea";
 
 export const action = async ({ request }: Route.ActionArgs) => {
   const formData = await request.formData();
@@ -23,10 +35,11 @@ export const action = async ({ request }: Route.ActionArgs) => {
   }
 
   if (submission.status === "success") {
-    const { name } = submission.value;
+    const { description, name } = submission.value;
 
     const { id } = await prisma.dough.create({
       data: {
+        description,
         name,
         user: {
           connect: {
@@ -52,25 +65,38 @@ export default function CreateDough({ actionData }: Route.ComponentProps) {
     <Page>
       <PageHeader>
         <PageTitle>Create Dough</PageTitle>
-        <PageIntro>
-          Please use a unique name for the dough. On the next page you will be
-          able to specify a description and assign ingredients.
-        </PageIntro>
+        <PageIntro>A dough is an integral part of a pizza.</PageIntro>
       </PageHeader>
       <Form {...getFormProps(form)} method="post" onSubmit={form.onSubmit}>
         <div className="grid gap-6">
           <Card>
+            <CardHeader>
+              <CardTitle>General Information</CardTitle>
+              <CardDescription>
+                Use a unique name for the dough. A description is optional.
+              </CardDescription>
+            </CardHeader>
             <CardContent>
-              <div className="grid gap-3">
-                <Label htmlFor={fields.name.id}>Name</Label>
-                <Input
-                  {...getInputProps(fields.name, { type: "text" })}
-                  autoFocus
-                />
-                <ErrorList
-                  errors={fields.name.errors}
-                  id={fields.name.errorId}
-                />
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor={fields.name.id}>Name*</Label>
+                  <Input
+                    {...getInputProps(fields.name, { type: "text" })}
+                    autoFocus
+                  />
+                  <ErrorList
+                    errors={fields.name.errors}
+                    id={fields.name.errorId}
+                  />
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor={fields.description.id}>Description</Label>
+                  <Textarea {...getTextareaProps(fields.description)} />
+                  <ErrorList
+                    errors={fields.description.errors}
+                    id={fields.description.errorId}
+                  />
+                </div>
               </div>
             </CardContent>
           </Card>
@@ -80,7 +106,7 @@ export default function CreateDough({ actionData }: Route.ComponentProps) {
               type="submit"
               variant="default"
             >
-              Next
+              Save
             </Button>
           </div>
         </div>
