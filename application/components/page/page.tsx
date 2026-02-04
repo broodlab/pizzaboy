a9b2ec32a3409b1ce758bcb7d2cfeb69b0d96f5a
pf <peterbugiel@gmail.com>
2026-02-04 21:37:34 +0100

    style(application): Give the page content more width.

--- application/components/page/page.tsx
+++ application/components/page/page.tsx
@@ -3,9 +3,7 @@ import type { FC, PropsWithChildren } from "react";
 export const Page: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="flex flex-col items-center">
-      <div className="flex w-full flex-col gap-8 lg:w-lg xl:w-xl 2xl:w-2xl">
-        {children}
-      </div>
+      <div className="flex w-full flex-col gap-8 lg:w-2xl">{children}</div>
     </div>
   );
 };
