f604ba6009d2bb16a8fef4584f5eaead7d28cc8d
pf <peterbugiel@gmail.com>
2026-02-05 22:35:53 +0100

    feat('Label' component): 'required' prop optionally letting the 'Label' display an asterisk.

--- application/components/label.tsx
+++ application/components/label.tsx
@@ -3,11 +3,11 @@ import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
 
 import { cn } from "~/utils/create-class-name";
+import { AsteriskIcon } from "lucide-react";
 
-export const Label: FC<ComponentProps<typeof LabelPrimitive.Root>> = ({
-  className,
-  ...props
-}) => {
+export const Label: FC<
+  ComponentProps<typeof LabelPrimitive.Root> & { required?: boolean }
+> = ({ children, className, required, ...props }) => {
   return (
     <LabelPrimitive.Root
       className={cn(
@@ -16,6 +16,11 @@ export const Label: FC<ComponentProps<typeof LabelPrimitive.Root>> = ({
       )}
       data-slot="label"
       {...props}
-    />
+    >
+      {children}
+      {required && (
+        <AsteriskIcon className="-mt-2 -ml-2 text-red-500" size={12} />
+      )}
+    </LabelPrimitive.Root>
   );
 };
