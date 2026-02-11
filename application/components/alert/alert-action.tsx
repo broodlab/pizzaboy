3d8f7b91683dbe33f73ea38dc0d8999fd9fec488
pf <peterbugiel@gmail.com>
2026-02-11 22:02:33 +0100

    feat('Alert' component): 'AlertAction' component allows specifying an action in the top-right corner.

---
+++ application/components/alert/alert-action.tsx
@@ -0,0 +1,16 @@
+import type { ComponentProps, FC } from "react";
+import * as React from "react";
+import { cn } from "~/utils/create-class-name";
+
+export const AlertAction: FC<ComponentProps<"div">> = ({
+  className,
+  ...props
+}) => {
+  return (
+    <div
+      className={cn("absolute top-2 right-2", className)}
+      data-slot="alert-title"
+      {...props}
+    />
+  );
+};
