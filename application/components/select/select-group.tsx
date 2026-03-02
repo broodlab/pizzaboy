127fd66f1b32dea72fdc65c9db6b355ac090701f
pf <peterbugiel@gmail.com>
2026-03-02 22:27:49 +0100

    chore(application): Get rid of the unused 'Select' component.

--- application/components/select/select-group.tsx
+++
@@ -1,8 +0,0 @@
-import { type ComponentProps, type FC } from "react";
-import * as SelectPrimitive from "@radix-ui/react-select";
-
-export const SelectGroup: FC<ComponentProps<typeof SelectPrimitive.Group>> = ({
-  ...props
-}) => {
-  return <SelectPrimitive.Group data-slot="select-group" {...props} />;
-};
--- application/components/select/select-value.tsx
+++
@@ -1,8 +0,0 @@
-import type { ComponentProps, FC } from "react";
-import * as SelectPrimitive from "@radix-ui/react-select";
-
-export const SelectValue: FC<ComponentProps<typeof SelectPrimitive.Value>> = ({
-  ...props
-}) => {
-  return <SelectPrimitive.Value data-slot="select-value" {...props} />;
-};
