4db67f11ee3abe0dc61056da74e13ac1b78b8d0e
pf <peterbugiel@gmail.com>
2026-03-14 21:44:37 +0100

    refactor('Separator' component): Migration to Base UI.

--- application/components/separator.tsx
+++ application/components/separator.tsx
@@ -1,22 +1,20 @@
-import type { ComponentProps, FC } from "react";
-import * as SeparatorPrimitive from "@radix-ui/react-separator";
+import type { FC } from "react";
+import { Separator as SeparatorPrimitive } from "@base-ui/react/separator";
 
 import { cn } from "~/utils/create-class-name";
 
-export const Separator: FC<ComponentProps<typeof SeparatorPrimitive.Root>> = ({
+export const Separator: FC<SeparatorPrimitive.Props> = ({
   className,
-  decorative = true,
   orientation = "horizontal",
   ...props
 }) => {
   return (
-    <SeparatorPrimitive.Root
+    <SeparatorPrimitive
       className={cn(
-        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+        "bg-border shrink-0 data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch",
         className,
       )}
       data-slot="separator"
-      decorative={decorative}
       orientation={orientation}
       {...props}
     />
