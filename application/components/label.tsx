5dbc5d57edffc34f9d5e46c5a6a8ef9cef64d4bb
pf <peterbugiel@gmail.com>
2026-03-20 22:48:26 +0100

    refactor('Label' component): Migration to Base UI.

--- application/components/label.tsx
+++ application/components/label.tsx
@@ -1,14 +1,16 @@
 import type { ComponentProps, FC } from "react";
-import * as LabelPrimitive from "@radix-ui/react-label";
 
 import { cn } from "~/utils/create-class-name";
 import { AsteriskIcon } from "lucide-react";
 
-export const Label: FC<
-  ComponentProps<typeof LabelPrimitive.Root> & { required?: boolean }
-> = ({ children, className, required, ...props }) => {
+export const Label: FC<ComponentProps<"label"> & { required?: boolean }> = ({
+  children,
+  className,
+  required,
+  ...props
+}) => {
   return (
-    <LabelPrimitive.Root
+    <label
       className={cn(
         "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
         className,
@@ -20,6 +22,6 @@ export const Label: FC<
       {required && (
         <AsteriskIcon className="-mt-2 -ml-2 text-red-500" size={12} />
       )}
-    </LabelPrimitive.Root>
+    </label>
   );
 };
