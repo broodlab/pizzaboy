a1841450c65a55ac75db69da5f8508c10fd98dec
pf <peterbugiel@gmail.com>
2026-04-10 21:49:47 +0200

    feat(application): 'ButtonContainer' component.

---
+++ application/components/button-container.tsx
@@ -0,0 +1,31 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "~/utils/create-class-name";
+import type { ComponentProps, FC } from "react";
+
+const buttonContainerVariants = cva(
+  "flex flex-col-reverse gap-2 sm:flex-row [&_svg]:invisible sm:[&_svg]:visible",
+  {
+    defaultVariants: {
+      alignment: "left",
+    },
+    variants: {
+      alignment: {
+        left: "sm:justify-start",
+        right: "sm:justify-end",
+      },
+    },
+  },
+);
+
+export const ButtonContainer: FC<
+  ComponentProps<"div"> & VariantProps<typeof buttonContainerVariants>
+> = ({ className, alignment = "left", ...props }) => {
+  return (
+    <div
+      className={cn(buttonContainerVariants({ alignment }), className)}
+      data-slot="button-container"
+      role="group"
+      {...props}
+    />
+  );
+};
